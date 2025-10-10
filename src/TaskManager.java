f33e9b96c04695dcebbdfcf48d41020697eb174a
alonazrnko <alonazrnko@gmail.com>
2025-10-10 17:10:09 +0300

    initial with commit

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -2,23 +2,33 @@ import java.util.ArrayList;
 
 public interface TaskManager {
     ArrayList<Task> getTasks();
+
     ArrayList<Epic> getEpics();
+
     ArrayList<Subtask> getSubtasks();
 
     Task getTask(int id);
+
     Epic getEpic(int id);
+
     Subtask getSubtask(int id);
 
     void createTask(Task task);
+
     void createEpic(Epic epic);
+
     void createSubtask(Subtask subtask);
 
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
 
     void deleteTaskById(int id);
+
     void deleteEpicById(int id);
+
     void deleteSubtaskById(int id);
 
     ArrayList<Subtask> getEpicSubtasks(int epicId);
